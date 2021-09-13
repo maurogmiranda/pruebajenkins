d1bdf4913b9ddc5b0722fff1487342dbe873a213
maurogmiranda <maurogmiranda185@gmail.com>
2021-09-13 19:38:43 -0300

    holis

---
+++ maven-adderapp/src/main/java/com/example/clase22a/Clase22aApplication.java
@@ -0,0 +1,13 @@
+package com.example.clase22a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clase22aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Clase22aApplication.class, args);
+	}
+
+}
---
+++ maven-adderapp/src/main/java/com/example/clase22a/controller/PacienteController.java
@@ -0,0 +1,15 @@
+package com.example.clase22a.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PacienteController {
+
+    @GetMapping("/index")
+    public String welcome(Model model) {
+        model.addAttribute("nombre", "roberto");
+        return "index";
+    }
+}
